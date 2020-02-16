d9fcd2c321ffe950d7957d511d0ee441d34bc15e
Hlamidomonada <60792528+Hlamidomonada@users.noreply.github.com>
2020-02-16 19:09:05 +0300

    Commit##1

--- conviction/Source.cpp
+++ conviction/Source.cpp
@@ -2,7 +2,17 @@
 
 int main()
 {
-	std::cout << " Text #1 ";
+	std::cout << "Text #1"<< std::endl;
+	std::cout << "Text #2"<< std::endl;
 
 	return 0;
+}
+
+void printGoodbye()
+{
+	std::cout << "Goodbye";
+}
+
+void printHello() {
+	std::cout << "Hello";
 }
